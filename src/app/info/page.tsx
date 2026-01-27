14e4a5614d9080828de073e193d0af15c6dee6f7
simonfraserduncan <simonfraserduncan@gmail.com>
2026-01-27 08:05:07 -0600

    Increase section spacing on info page

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -452,7 +452,7 @@ export default function InfoPage() {
       {/* 2-column layout: content takes more space, image is smaller */}
       <section className="grid gap-10 lg:grid-cols-[1fr_440px] lg:gap-24">
         {/* Left column: intro (unanimated) + table immediately below */}
-        <div className="space-y-8 max-w-[768px]">
+        <div className="space-y-12 max-w-[768px]">
           <div className="space-y-4">
             {/* Spacer for visual alignment with home page */}
             <div className="h-9" aria-hidden="true" />
